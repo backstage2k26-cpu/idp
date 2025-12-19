d0f49664104f5a0a4993e8b124fd0c91c34640d9
jayachandran.goli <jayachandran.goli@macys.com>
2025-12-19 18:21:22 +0530

    initial commit

---
+++ packages/backend/src/index.ts
@@ -0,0 +1,131 @@
+/*
+ * Hi!
+ *
+ * Note that this is an EXAMPLE Backstage backend. Please check the README.
+ *
+ * Happy hacking!
+ */
+
+import { createBackend } from '@backstage/backend-defaults';
+import { createBackendModule } from '@backstage/backend-plugin-api';
+import { githubAuthenticator } from '@backstage/plugin-auth-backend-module-github-provider';
+import {
+ authProvidersExtensionPoint,
+ createOAuthProviderFactory,
+} from '@backstage/plugin-auth-node';
+import { stringifyEntityRef } from '@backstage/catalog-model';
+
+
+
+const backend = createBackend();
+
+backend.add(import('@backstage/plugin-app-backend'));
+backend.add(import('@backstage/plugin-proxy-backend'));
+
+// scaffolder plugin
+backend.add(import('@backstage/plugin-scaffolder-backend'));
+backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
+backend.add(
+  import('@backstage/plugin-scaffolder-backend-module-notifications'),
+);
+
+// techdocs plugin
+backend.add(import('@backstage/plugin-techdocs-backend'));
+
+// ldap auth backend module
+backend.add(import('@immobiliarelabs/backstage-plugin-ldap-auth-backend'));
+
+// auth plugin
+backend.add(import('@backstage/plugin-auth-backend'));
+
+// See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
+backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
+
+// See https://backstage.io/docs/auth/guest/provider
+
+// catalog plugin
+backend.add(import('@backstage/plugin-catalog-backend'));
+backend.add(
+  import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
+);
+
+// See https://backstage.io/docs/features/software-catalog/configuration#subscribing-to-catalog-errors
+backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
+
+// permission plugin
+backend.add(import('@backstage/plugin-permission-backend'));
+// See https://backstage.io/docs/permissions/getting-started for how to create your own permission policy
+backend.add(
+  import('@backstage/plugin-permission-backend-module-allow-all-policy'),
+);
+
+// search plugin
+backend.add(import('@backstage/plugin-search-backend'));
+
+// search engine
+// See https://backstage.io/docs/features/search/search-engines
+backend.add(import('@backstage/plugin-search-backend-module-pg'));
+
+// search collators
+backend.add(import('@backstage/plugin-search-backend-module-catalog'));
+backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
+
+// kubernetes plugin
+backend.add(import('@backstage/plugin-kubernetes-backend'));
+
+// notifications and signals plugins
+backend.add(import('@backstage/plugin-notifications-backend'));
+backend.add(import('@backstage/plugin-signals-backend'));
+
+
+const customAuth = createBackendModule({
+ // This ID must be exactly "auth" because that's the plugin it targets
+ pluginId: 'auth',
+ // This ID must be unique, but can be anything
+ moduleId: 'custom-auth-provider',
+ register(reg) {
+   reg.registerInit({
+     deps: { providers: authProvidersExtensionPoint },
+     async init({ providers }) {
+       providers.registerProvider({
+         // This ID must match the actual provider config, e.g. addressing
+         // auth.providers.github means that this must be "github".
+         providerId: 'github',
+         // Use createProxyAuthProviderFactory instead if it's one of the proxy
+         // based providers rather than an OAuth based one
+         factory: createOAuthProviderFactory({
+           authenticator: githubAuthenticator,
+           async signInResolver(info, ctx) {
+             console.log('============= github info ==========');
+             console.log(info);
+             // 1️⃣ Get GitHub username (always present)
+             const username =
+               info.result?.fullProfile?.username ??
+               info.profile?.displayName?.replace(/\s+/g, '').toLowerCase();
+             if (!username) {
+               throw new Error('GitHub username not found');
+             }
+             // 2️⃣ Build Backstage user entity ref
+             const userEntity = stringifyEntityRef({
+               kind: 'User',
+               name: username,
+               namespace: 'default',
+             });
+             // 3️⃣ Issue token
+             return ctx.issueToken({
+               claims: {
+                 sub: userEntity,
+                 ent: [userEntity],
+               },
+             });
+           }
+         }),
+       });
+     },
+   });
+ },
+});
+backend.add(customAuth);
+
+backend.start();
+ 
